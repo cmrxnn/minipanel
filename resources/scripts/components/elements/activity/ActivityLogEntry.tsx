393094a598767c527f9ef64da08bf427a4efae6c
cmrxnn <cmrxn2005@gmail.com>
2022-12-21 15:35:53 -0800

    (frontend): remove unused deps

--- resources/scripts/components/elements/activity/ActivityLogEntry.tsx
+++ resources/scripts/components/elements/activity/ActivityLogEntry.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Tooltip from '@/components/elements/tooltip/Tooltip';
-import Translate from '@/components/elements/Translate';
 import { format, formatDistanceToNowStrict } from 'date-fns';
 import { ActivityLog } from '@definitions/user';
 import ActivityLogMetaButton from '@/components/elements/activity/ActivityLogMetaButton';
@@ -10,38 +9,15 @@ import classNames from 'classnames';
 import style from './style.module.css';
 import Avatar from '@/components/Avatar';
 import useLocationHash from '@/plugins/useLocationHash';
-import { getObjectKeys, isObject } from '@/lib/objects';
 
 interface Props {
     activity: ActivityLog;
     children?: React.ReactNode;
 }
 
-function wrapProperties(value: unknown): any {
-    if (value === null || typeof value === 'string' || typeof value === 'number') {
-        return `<strong>${String(value)}</strong>`;
-    }
-
-    if (isObject(value)) {
-        return getObjectKeys(value).reduce((obj, key) => {
-            if (key === 'count' || (typeof key === 'string' && key.endsWith('_count'))) {
-                return { ...obj, [key]: value[key] };
-            }
-            return { ...obj, [key]: wrapProperties(value[key]) };
-        }, {} as Record<string, unknown>);
-    }
-
-    if (Array.isArray(value)) {
-        return value.map(wrapProperties);
-    }
-
-    return value;
-}
-
 export default ({ activity, children }: Props) => {
     const { pathTo } = useLocationHash();
     const actor = activity.relationships.actor;
-    const properties = wrapProperties(activity.properties);
 
     return (
         <div className={'grid grid-cols-10 py-4 border-b-2 border-gray-800 last:rounded-b last:border-0 group'}>
@@ -77,9 +53,7 @@ export default ({ activity, children }: Props) => {
                             {children}
                         </div>
                     </div>
-                    <p className={style.description}>
-                        <Translate ns={'activity'} values={properties} i18nKey={activity.event.replace(':', '.')} />
-                    </p>
+                    <p className={style.description}>{activity.event.replace(':', '.')}</p>
                     <div className={'mt-1 flex items-center text-sm'}>
                         {activity.ip && (
                             <span>
